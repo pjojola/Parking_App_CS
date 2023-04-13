c61cc2935bbda2062bb4f6abaccf6efb488f24d8
Bianka B <123679371+bboudre@users.noreply.github.com>
2023-04-12 23:17:03 -0600

    Finished Campus Map and parts of UI

--- parking_app/Views/ContentView.swift
+++ parking_app/Views/ContentView.swift
@@ -9,35 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 75) {
-            Text("Parking App")
-                .font(.system(size: 55))
-                .padding(50)
+        NavigationView{
+            VStack(spacing: 30) {
+                Text("Aggie Park")
+                    .font(.system(size: 50))
+                    .fontWeight(.bold)
+                    .padding(.bottom, 108.0)
                 
-            Button {
-            } label: {
-                Text("Find Parking")
-                   .padding()
-            }
-            
-            Button {
-            } label: {
-                Text("Campus Map")
-                   .padding()
-            }
-            
-            Button {
-            } label: {
-                Text("Settings")
-                   .padding()
-            }
-            
-            Text("Scuff Development Inc.")
-                .font(.system(size: 25))
-                .padding(50)
-        }
-    }
-}
+                NavigationLink(destination: DetailView() ) {
+                    Text("Find Parking")
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .padding()
+                        .background(Color("Crimson"))
+                        .foregroundColor(.white)
+                        .cornerRadius(40)
+                        .padding(.horizontal,30)
+                }
+                
+                NavigationLink(destination: CampusMap() ) {
+                    Text("Campus Map")
+                       .frame(minWidth: 0, maxWidth: .infinity)
+                       .padding()
+                       .background(Color("Crimson"))
+                       .foregroundColor(.white)
+                       .cornerRadius(40)
+                       .padding(.horizontal,30)
+                }
+                NavigationLink(destination: Settings() ) {
+                    Text("Settings")
+                       .frame(minWidth: 0, maxWidth: .infinity)
+                       .padding()
+                       .background(Color("Crimson"))
+                       .foregroundColor(.white)
+                       .cornerRadius(40)
+                       .padding(.horizontal,30)
+                }
+                                
+                Text("Scuff Development Inc.")
+                    .font(.footnote)
+                    .padding(.top, 100.0)
+            } // v stack
+        } // nav view
+        .accentColor(Color("Crimson"))
+    } // var body
+} // content view
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
