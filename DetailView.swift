5366e9173d6c91c011ea0946cd0ae8f4b2ff7e8d
Bianka B <123679371+bboudre@users.noreply.github.com>
2023-04-20 13:02:09 -0600

    Updated with user location

--- DetailView.swift
+++ DetailView.swift
@@ -6,9 +6,86 @@
 //
 
 import SwiftUI
+import MapKit
+
+// search building
+// show lots closet to building and the available spots open
+// show navigation to parking lot
+
+// available spots
+// distance
+
+// calculate who is closet to parking lot and update
+
+
+
 
 struct DetailView: View {
-  var body: some View {
-    Text("Loading...")
-  }
-}
+    @State private var searchText = ""
+    @StateObject private var viewModel = DetailViewModel()
+
+    
+    var body: some View {
+        Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
+            .accentColor(Color(.systemPink))
+            .onAppear {
+                viewModel.checkLocationIsEnabled()
+            }
+        
+        
+    NavigationStack {
+        Text("\(searchText)")
+            .navigationTitle("Where to?")
+    }
+    .searchable(text: $searchText, prompt: "Search buildings" )
+        
+        
+    } // body view
+} // detail view
+
+    
+final class DetailViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+            
+    @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 32.2793, longitude: -106.7491), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    var locationManager:CLLocationManager?
+    
+    func checkLocationIsEnabled() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager = CLLocationManager()
+            locationManager!.delegate = self
+            //locationManager?.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        }
+        else {
+            print("Location is off")
+        }
+    } // checkLocationIsEnabled
+    
+    
+    private func checkLocationAuth() {
+        guard let locationManager = locationManager else { return }
+        
+        switch locationManager.authorizationStatus {
+    
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            print("Your location is restricted likely de to parental controls")
+        case .denied:
+            print("App location is denied. Go to settings to change it")
+        case .authorizedAlways, .authorizedWhenInUse:
+            region = MKCoordinateRegion(center: locationManager.location!.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        @unknown default:
+            break
+        }
+        
+    } // checkLocationAuth
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuth()
+    }
+    
+    
+} // view model
+    
+
+
--- Settings.swift
+++ Settings.swift
@@ -8,7 +8,37 @@
 import SwiftUI
 
 struct Settings: View {
+    @State var recieve = false
+    @State var selection = 0
+    
+    private var identType = ["","Student", "Faculty/Staff"]
+    @State private var type = ""
+    
   var body: some View {
-    Text("Settings")
-  }
-}
+      Form {
+          Toggle(isOn: $recieve) {
+              Text("Recieve Notifications")
+          }
+          
+          Section(header: Text("Info") ) {
+              Picker(selection: $type, label: Text("Identification")) {
+                  ForEach (identType, id: \.self) {activity in
+                      Text(activity)
+                  }
+              }
+              if type == "Student" {
+                  Picker(selection: $selection, label: Text("Permit Type")) {
+                      Text("Commuter").tag(1)
+                      Text("Resident South").tag(2)
+                      Text("Resident North").tag(3)
+                      Text("None").tag(4)
+                  }
+
+              }
+          } // section info
+
+      }.navigationTitle("Settings")
+    
+  } // view body
+} // view settings
+
